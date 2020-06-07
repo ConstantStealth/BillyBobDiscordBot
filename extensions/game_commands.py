667be2d0ee4f5dda099fdc315622c99e0d7e03e6
GabeLile <31359332+GabeLile@users.noreply.github.com>
2020-06-07 06:42:25 -0500

    tstatus comnmand update

--- extensions/game_commands.py
+++ extensions/game_commands.py
@@ -9,7 +9,7 @@ class Game(commands.Cog):
 
     @commands.command(brief='- Displays the current status of the Terraria server.')
     async def tstatus(self, ctx):
-        await ctx.send('Pinging Terraria Server...')
+        await ctx.send('Pinging Terraria Server... ' 'This can take up to 30 seconds.')
         cmd = 'nc -vz -w 30 theboisterraria.ddns.net 7777'
         proc = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE,
                                                      stderr=asyncio.subprocess.PIPE)
@@ -17,9 +17,9 @@ class Game(commands.Cog):
         stdout, stderr = await proc.communicate()
 
         if proc.returncode == 0:
-            await ctx.send('Server is up.')
+            await ctx.send('The Terraria server is currently up.')
         else:
-            await ctx.send('Server is down.')
+            await ctx.send('The Terraria server is currently down.')
 
 def setup(bot):
     bot.add_cog(Game(bot))
