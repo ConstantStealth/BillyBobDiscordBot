50fa7c810e59a5532570a595ab5136033f8d98a2
GabeLile <31359332+GabeLile@users.noreply.github.com>
2020-06-07 15:56:22 -0500

    Tinfo addition and tstatus update
    Command tinfo created.
    
    Command tstatus updated to support variable IP and PORT, as well as deleting Pinging Terraria Server message after response.

--- extensions/game_commands.py
+++ extensions/game_commands.py
@@ -7,11 +7,15 @@ class Game(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    IP = '192.168.0.0'
+    PORT = '7777'
+
 #Checks the current status of the Terraria server
     @commands.command(brief='- Displays the current status of the Terraria server.')
     async def tstatus(self, ctx):
-        await ctx.send('Pinging Terraria Server... ' 'This can take up to 30 seconds.')
-        cmd = 'nc -vz -w 30 theboisterraria.ddns.net 7777'
+        message = 'Pinging Terraria Server... ' 'This can take up to 30 seconds.'
+        await ctx.send(message)
+        cmd = ('nc -vz -w 30 ' + self.IP +' ' + self.PORT)
         proc = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE,
                                                      stderr=asyncio.subprocess.PIPE)
 
@@ -19,8 +23,16 @@ class Game(commands.Cog):
 
         if proc.returncode == 0:
             await ctx.send('The Terraria server is currently up ' +ctx.message.author.mention)
+            await ctx.delete_message(message)
         else:
             await ctx.send('The Terraria server is currently down ' +ctx.message.author.mention)
+            await ctx.delete_message(message)
+
+#Displays the Terraria server IP and Port
+    @commands.command(brief='- Displays the Terraria Server IP and Port')
+    async def tinfo(self, ctx):
+        await ctx.send('Terraria Server IP: ' + self.IP +
+                       'Server Port: ' + self.PORT)
 
 def setup(bot):
     bot.add_cog(Game(bot))
