98a0c65b04aac1a5862197dc728df307e0a341f2
GabeLile <31359332+GabeLile@users.noreply.github.com>
2020-06-11 15:54:30 -0500

    Added TOKEN to import from confidential.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from discord.ext import commands
 import os
+from confidential import TOKEN
 
-#Get token.txt from token.txt.txt
-def read_token():
-    with open('token.txt', 'r') as f:
-        lines = f.readlines()
-        return lines[0].strip()
-token=read_token()
-
-#Command prefix
+# Command prefix
 bot = commands.Bot(command_prefix='.')
 
-#Load extensions
+# Load extensions
 for filename in os.listdir("./extensions"):
     if filename.endswith(".py") and filename != "__init__.py":
         bot.load_extension(f'extensions.{filename[:-3]}')
 
-#Run bot with token.txt passed
-bot.run(token)
+# Run bot with token.txt passed
+bot.run(TOKEN)
