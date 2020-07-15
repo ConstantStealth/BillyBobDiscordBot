55ec2804f8d6049c560a3423b1f20a64a1016903
GabeLile <31359332+GabeLile@users.noreply.github.com>
2020-07-15 18:31:03 -0500

    Updated to support .rolelist command

--- extensions/roles.py
+++ extensions/roles.py
@@ -17,7 +17,7 @@ class Roles(commands.Cog):
     @commands.check_any(commands.is_owner(), commands.has_role('Admin'))
     @commands.command()
     async def createrole(self, ctx, role: str, color: discord.Colour,
-                                 joinable=False):
+                                 joinable=True):
         conn = sqlite3.connect('databases/roles.db')
         c = conn.cursor()
         name = (role,)
@@ -25,11 +25,11 @@ class Roles(commands.Cog):
         data = c.fetchall()
         if len(data) > 0:
             await ctx.send('That role already exists.')
-        else:
+        elif joinable is True:
             print('Running createrole')
             await ctx.guild.create_role(name=role, color=color)
             print('Role created.')
-            if joinable is not False:
+            if joinable is True:
                 # Stash the role in the database.
                 data = (role, joinable)
                 c.execute('INSERT INTO roles VALUES (?,?)', data)
@@ -60,7 +60,7 @@ class Roles(commands.Cog):
             await ctx.send(f'{role_name} does not exist.')
 
     @commands.command()
-    async def subscribe(self, ctx, *, role_name: str):
+    async def joinrole(self, ctx, *, role_name: str):
         role = discord.utils.get(ctx.guild.roles, name=role_name)
         if role is not None:
             roles = ctx.author.roles
@@ -73,6 +73,34 @@ class Roles(commands.Cog):
         else:
             ctx.send('The role {role_name} doesn\'t seem to exist. Use '
                      '.roleslist to confirm.')
+
+    @commands.command()
+    async def rolelist(self, ctx):
+        conn = sqlite3.connect('databases/roles.db')
+        c = conn.cursor()
+        joinable = (True,)
+        search = c.execute('SELECT * FROM roles WHERE joinable=?', joinable)
+        roles = c.fetchall()
+        msg = ""
+        for role in roles:
+            msg = f'{msg}{role[0]}\n'
+        msg = f'```\n{msg}```'
+        await ctx.send('Below is a list of self-joinable roles: %s' % msg)
+
+    @commands.check_any(commands.is_owner(), commands.has_role('Admin'))
+    @commands.command()
+    async def updatedb(self, ctx):
+        await ctx.send('Committing and pushing database to GitHub... ')
+        cmd = 'nc -vz -w 30 '
+        proc = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE,
+                                                     stderr=asyncio.subprocess.PIPE)
+
+        stdout, stderr = await proc.communicate()
+
+        if proc.returncode == 0:
+            await ctx.send('The database has been updated to GitHub')
+        else:
+            await ctx.send('There was an error updating the database to GitHub')
 
 def setup(bot):
     bot.add_cog(Roles(bot))
