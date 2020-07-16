b1cb0da522086a526df126307c94aa9403c6c735
GabeLile <31359332+GabeLile@users.noreply.github.com>
2020-07-16 07:01:55 -0500

    Removed .test command

--- extensions/admin_commands.py
+++ extensions/admin_commands.py
@@ -5,10 +5,5 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(brief="test admin command")
-    @commands.check_any(commands.is_owner(), commands.has_role('Admin'))
-    async def admin(self, ctx):
-            await ctx.send('test')
-
 def setup(bot):
     bot.add_cog(Admin(bot))
--- extensions/roles.py
+++ extensions/roles.py
@@ -17,8 +17,7 @@ class Roles(commands.Cog):
         conn.close()
 
     @commands.check_any(commands.is_owner(), commands.has_role('Admin'))
-    @commands.command(name='CreateRole', brief='Creates a self-joinable role',
-                      help='Use the following syntax: .createrole [role name] [role color]')
+    @commands.command(name='createrole', brief='Creates a self-joinable role')
     async def createrole(self, ctx, role: str, color: discord.Colour,
                                  joinable=True):
         conn = sqlite3.connect('databases/roles.db')
@@ -42,8 +41,7 @@ class Roles(commands.Cog):
         role_cache_updated = True
 
     @commands.check_any(commands.is_owner(), commands.has_role('Admin'))
-    @commands.command(name='DeleteRole', brief='Deletes a self-joinable role',
-                      help='Use the following syntax: .deleterole [role name]')
+    @commands.command(name='deleterole', brief='Deletes a self-joinable role',)
     async def deleterole(self, ctx, *, role_name):
         role = discord.utils.get(ctx.guild.roles, name=role_name)
         if role is not None:
@@ -63,8 +61,7 @@ class Roles(commands.Cog):
         else:
             await ctx.send(f'{role_name} does not exist.')
 
-    @commands.command(name='JoinRole', brief='Join a self-joinable role',
-                      help='Use the following syntax: .joinrole [role name]',
+    @commands.command(name='joinrole', brief='Join a self-joinable role',
                       alias='subscribe')
     async def joinrole(self, ctx, *, role_name: str):
         role = discord.utils.get(ctx.guild.roles, name=role_name)
@@ -80,8 +77,7 @@ class Roles(commands.Cog):
             ctx.send('The role {role_name} doesn\'t seem to exist. Use '
                      '.roleslist to confirm.')
 
-    @commands.command(name='RoleList', brief='Displays a list of self-joinable roles',
-                      help='Use the following syntax: .rolelist',
+    @commands.command(name='rolelist', brief='Displays a list of self-joinable roles',
                       alias='roleslist')
     async def rolelist(self, ctx):
         conn = sqlite3.connect('databases/roles.db')
@@ -96,8 +92,7 @@ class Roles(commands.Cog):
         await ctx.send('Below is a list of self-joinable roles: %s' % msg)
 
     @commands.check_any(commands.is_owner(), commands.has_role('Admin'))
-    @commands.command(name='Backup', brief='Backup all new/updated bot files from VPS to GitHub',
-                      help='Use the following syntax: .backup')
+    @commands.command(name='backup', brief='Backup all new/updated bot files from VPS to GitHub')
     async def backup(self, ctx):
         await ctx.send('Committing and pushing updated files to GitHub... ')
         cmd = subprocess.run('git commit -am "Update" && git push', shell=True)
