b5bb744afbadbdc0350fedff623f1087dcd127c9
GabeLile <31359332+GabeLile@users.noreply.github.com>
2020-07-16 17:42:53 -0500

    Added purge command

--- extensions/admin_commands.py
+++ extensions/admin_commands.py
@@ -5,5 +5,11 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
+    @commands.check_any(commands.is_owner(), commands.has_role('Admin'))
+    @commands.command(name='purge', brief='Purges [X] amount of messages from the chat')
+    async def purge(self, ctx, amount = 0):
+        await ctx.channel.purge(limit = amount + 1)
+
 def setup(bot):
     bot.add_cog(Admin(bot))
