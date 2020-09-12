1e070d3d1a39672fe39b4fe75c058fad6cc32ecf
GabeLile <31359332+GabeLile@users.noreply.github.com>
2020-09-12 13:47:34 -0500

    Created prune command to kick inactive members
    Prune command will kick members inactive for 30 or more days.

--- extensions/admin_commands.py
+++ extensions/admin_commands.py
@@ -7,8 +7,15 @@ class Admin(commands.Cog):
 
     @commands.check_any(commands.is_owner(), commands.has_role('Admin'))
     @commands.command(name='purge', brief='Purges [X] amount of messages from the chat')
-    async def purge(self, ctx, amount = 0):
-        await ctx.channel.purge(limit = amount + 1)
+    async def purge(self, ctx, amount=0):
+        await ctx.channel.purge(limit=amount + 1)
+
+    @commands.check_any(commands.is_owner())
+    @commands.command(name='prune', brief='Purges inactive members from the Discord server')
+    async def prune(self, ctx):
+        prune_count = await ctx.guild.prune_members(days=30, compute_prune_count=True, roles=ctx.guild.roles or None,
+                                      reason='Pruned for inactivity')
+        await ctx.send(f'Pruned {prune_count} inactive members from the server.')
 
 def setup(bot):
     bot.add_cog(Admin(bot))
